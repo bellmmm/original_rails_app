de342a493ca6fed3db755ce9ade5826433fac58b
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-07-20 08:12:45 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,10 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+    
+
   # Add more helper methods to be used by all tests here...
 end
