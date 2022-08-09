95d012637d20a60f8b4d377ea80a59e915052b75
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-08-09 14:21:45 +0000

    Add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,71 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user,          only: [:edit, :update]
+  before_action :valid_user,        only: [:edit, :update]
+  before_action :check_expiration,  only: [:edit, :update]  # （1）再設定の有効期限確認
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+  
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?            # （3）新パスワードの空文字列を拒否
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update(user_params)              # （4）新パスワードが正しければ更新
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'                               # （2）無効パスワードは失敗させ理由を表示
+    end
+  end
+
+  
+  
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    #beforeフィルター
+    
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] ="Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   # get 'sessions/new'
   # get 'users/new'
   root 'static_pages#home'
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
