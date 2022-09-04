4fdf5c31ad234c985afef4c835e5d86dd0e08b58
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-04 11:00:05 +0000

    [revert] Revert to commit before micropost imp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only:  :destroy
   
@@ -10,7 +9,6 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated
   end
 
@@ -48,21 +46,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def following
-    @title = "Following"
-    @user =  User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
-  end
-
-  def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
-  end
-  
-
 
   private
 
@@ -72,15 +55,15 @@ class UsersController < ApplicationController
   
   # beforeアクション
 
-    # ログイン済みユーザーかどうか確認→ApplicationControllerに移行済
-    # def logged_in_user
-    #   unless logged_in?
-    #     store_location
-    #     store_location
-    #     flash[:danger] = "Please log in."
-    #     redirect_to login_url
-    #   end
-    # end
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
     # 正しいユーザーかどうか確認
     def correct_user
