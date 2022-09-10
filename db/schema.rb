9364083443124dbdfb57b0adbe20bb1c8f75a124
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-10 07:31:08 +0000

    [fix] renamed column in troduct model to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_09_100536) do
+ActiveRecord::Schema.define(version: 2022_09_10_072347) do
 
   create_table "products", force: :cascade do |t|
-    t.string "product_name"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_name"], name: "index_products_on_product_name", unique: true
+    t.index ["name"], name: "index_products_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
