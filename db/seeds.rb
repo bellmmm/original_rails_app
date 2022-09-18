6ebcba47d05ce6c6de39e89362c4e8c9ba295f3b
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-18 08:29:58 +0000

    [docs] changed seeds and fixture of products

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,27 @@ User.create!(name:  "Example User",
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password,
-    activated: true,
-    activated_at: Time.zone.now)
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+#メインのサンプルプロダクトを1つ作成する
+Product.create!(name:  "Sample_product01")
+
+# 追加のプロダクトをまとめて生成する
+98.times do |n|
+  if n < 8
+    name  = "Sample_product0#{n+2}"
+    Product.create!(name:  name)
+  else
+    name  = "Sample_product#{n+2}"
+    Product.create!(name:  name)
+  end
 end
