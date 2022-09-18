9356ba3d6da1ee9b05bbc7038d75863856f52ae5
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-18 08:25:24 +0000

    [test] product_controller (show)

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+   @product = products(:test01)
+  end
+
+  
   test "should get show" do
-    get products_show_url
+    get product_path(@product)
     assert_response :success
   end
 
