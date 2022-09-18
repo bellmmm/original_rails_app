86a8b8256569e0b94ba2a5edeca6159076b3ac70
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-18 08:24:36 +0000

    [feat] products on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @products = Product.paginate(page: params[:page], per_page:12)
+    end
   end
 
   def help
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length:{maximum:255},
                           uniqueness: true
+  default_scope -> { order(:id)}
 end
