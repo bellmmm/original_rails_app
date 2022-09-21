77c4a0a78b7a4fd844d401e7b3ea70f69b3ed564
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-21 05:34:40 +0000

    [feat] created test product display in tile style

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @products = Product.paginate(page: params[:page], per_page:12)
+      @test_products = Product.all[1..12]
     end
   end
 
