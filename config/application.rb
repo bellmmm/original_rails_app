7fd1b2ab0aa33de6c8fa5d8858e38eb681ef0d80
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-09-22 07:32:23 +0000

    [feat] added Ajax to dislike button

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
