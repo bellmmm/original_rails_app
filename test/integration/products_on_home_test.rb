cefd0592f8b3a7717d171a000032ac1e76294e70
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-10-11 15:03:52 +0000

    [test] adjusted test to new @test_products

--- test/integration/products_on_home_test.rb
+++ test/integration/products_on_home_test.rb
@@ -23,12 +23,25 @@ class ProductsOnHomeTest < ActionDispatch::IntegrationTest
   #   end
   # end
 
-test "home for logged in users(test)" do
+test "home for logged in users" do
     log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
     # assert_select "div.products", count: 1
-    test_products = Product.all[1..12]
+    test_products =  [
+      Product.all[0],
+      Product.all[1],
+      Product.all[2],
+      Product.all[5],
+      Product.all[8],
+      Product.all[9],
+      Product.all[14],
+      Product.all[15],
+      Product.all[17],
+      Product.all[19],
+      Product.all[23],
+      Product.all[24]
+    ]
     test_products.each do |product|
       assert_select "div#test_product-#{product.id}"
     end
@@ -37,7 +50,7 @@ test "home for logged in users(test)" do
   
 
   test "order of products should be ascending by id" do
-    assert_equal [products(:test01),products(:test02),products(:test03)].min, Product.first
+    assert_equal Product.all.min, Product.first
   end
 
 end
