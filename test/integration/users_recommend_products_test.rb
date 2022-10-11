7f804a8b2878faad5dec5d3422988c1090182bc7
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-10-11 15:07:50 +0000

    [test] adjusted to new fixture

--- test/integration/users_recommend_products_test.rb
+++ test/integration/users_recommend_products_test.rb
@@ -9,13 +9,13 @@ class UsersRecommendProductsTest < ActionDispatch::IntegrationTest
     @other_product = products(:test02)
     @third_product = products(:test03)
     @feature = features(:feat_1)
-    # dislikeのfixtureで設定されているtest01とのdislike関係を削除する
+    # dislikeのfixtureで設定されているmichaelとtest01とのdislike関係を削除する
     dislike = @user.dislikes.find_by(product_id: @product.id)
     delete dislike_path(dislike)
-    assert_not @user.dislike_products.include?(@product)
   end
 
-  test "should get features which users don't choose as dislike features (twice)" do
+  test "should get user's dislike features" do
+    assert_not @user.dislike_products.include?(@product)
     # dislikeに設定している要素が2つ未満の場合は表示しない
     post dislikes_path, params: { product_id: @product.id }
     get user_path(@user)
@@ -26,13 +26,20 @@ class UsersRecommendProductsTest < ActionDispatch::IntegrationTest
     assert_match CGI.escapeHTML(@feature.feature), response.body
   end
 
-  test "should get users recommended products" do
+  test "should get user's recommended products" do
+    # assert_not @user.dislike_products.include?(@product)
     # test01とtest02とのdislike関係を作成 
     post dislikes_path, params: { product_id: @product.id }
     post dislikes_path, params: { product_id: @other_product.id }
+    all_body=""
     get user_path(@user)
-    assert_no_match CGI.escapeHTML(@product.name), response.body
-    assert_match CGI.escapeHTML(@third_product.name), response.body
+    all_body+=response.body
+    get user_path(@user),params:{page:2}
+    all_body+=response.body
+    get user_path(@user),params:{page:3}
+    all_body+=response.body
+    assert_no_match CGI.escapeHTML(@product.name), all_body
+    assert_match CGI.escapeHTML(@third_product.name), all_body
   end
 
 end
