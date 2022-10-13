8fed7ae975aaf4889cde2d852eb10b777a6e1530
bellmmm <91827558+bellmmm@users.noreply.github.com>
2022-10-13 06:30:23 +0000

    [chore] changed application name in title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,30 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "PICK OUT"
   end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "PICK OUT"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | PICK OUT"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | PICK OUT"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | PICK OUT"
   end
 end
